53b1598e7c7298b5c2143cc3da6c88b10f74266f
Victor Cuc <victorcuc99@yahoo.com>
2021-09-29 16:02:06 +0300

    Initial Commit

---
+++ FlagStack/FlagStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagStackApp.swift
+//  FlagStack
+//
+//  Created by Victor Cuc on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlagStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
